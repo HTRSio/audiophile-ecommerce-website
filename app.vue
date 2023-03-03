82407c631a3cc15cba28024195db7333de3ff92f
HTRSio <info@htrs.io>
2023-03-03 20:45:27 +0100

    Add some styles to body tag, change to NuxtLayout and NuxtPage

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+import "@fontsource/manrope";
+
+useHead({
+  bodyAttrs: {
+    class: ["antialiased", "debug-screens", "font-sans", "text-15"],
+  },
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
