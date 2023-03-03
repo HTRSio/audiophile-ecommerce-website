c7aa397989ed41350c762f79e511ed23351ef759
HTRSio <info@htrs.io>
2023-03-03 20:52:01 +0100

    Add hero for homepage as component

---
+++ components/widgets/hero.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    default() {},
+  },
+});
+</script>
+
+<template>
+  <section class="w-full h-full bg-overlay">
+    <div
+      class="mx-auto h-[600px] tablet:h-[729px] max-w-screen-mobile tablet:max-w-screen-tablet desktop:max-w-screen-desktop"
+    >
+      <div
+        class="w-full h-full bg-center bg-no-repeat bg-cover bg-opacity-50 border-solid border-seventh bg-[url('../home/mobile/image-header.jpg')] tablet:bg-[url('../home/tablet/image-header.jpg')] desktop:bg-[url('../home/desktop/image-hero.jpg')]"
+      >
+        <div class="flex h-full items-center">
+          <div
+            class="mx-auto desktop:mx-0 w-[398px] text-center desktop:text-left"
+          >
+            <AtomsOverline class="text-white opacity-50">
+              {{ props.data.subTitle }}
+            </AtomsOverline>
+            <AtomsHeading class="text-white py-[1.5rem]">
+              {{ props.data.title }}
+            </AtomsHeading>
+
+            <p
+              class="text-15 text-white opacity-75 w-11/12 font-medium pb-[40px]"
+            >
+              {{ props.data.content }}
+            </p>
+            <div class="flex justify-center desktop:justify-start">
+              <AtomsButtonOne>
+                {{ props.data.buttonText }}
+              </AtomsButtonOne>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
