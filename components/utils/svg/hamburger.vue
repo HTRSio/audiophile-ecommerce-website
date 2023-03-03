e950b35872c19374ce6c93459e6065735dbaade4
HTRSio <info@htrs.io>
2023-03-03 20:47:30 +0100

    Navigatio - Hamburger as Component

---
+++ components/utils/svg/hamburger.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const props = defineProps({
+  height: {
+    type: Number,
+    default: 15,
+  },
+  width: {
+    type: Number,
+    default: 16,
+  },
+});
+</script>
+
+<template>
+  <svg
+    :height="props.height"
+    :width="props.width"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g fill="#FFF" fill-rule="evenodd">
+      <path d="M0 0h16v3H0zM0 6h16v3H0zM0 12h16v3H0z" />
+    </g>
+  </svg>
+</template>
+
+<style scoped></style>
