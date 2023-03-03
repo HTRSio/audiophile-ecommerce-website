96a45119d742ffbe43cb0773a52168b41d53668b
HTRSio <info@htrs.io>
2023-03-03 20:50:06 +0100

    Add overline component

---
+++ components/atoms/overline.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const props = defineProps({
+  tag: {
+    type: String,
+    default: "div",
+    validator(value) {
+      return ["span", "div"].includes(value);
+    },
+  },
+});
+</script>
+
+<template>
+  <component :is="props.tag">
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+div,
+span {
+  @apply text-14 uppercase;
+}
+</style>
