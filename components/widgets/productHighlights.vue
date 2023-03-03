9111e9119633fcdae04872dbd38570961aef7ddc
HTRSio <info@htrs.io>
2023-03-03 20:55:23 +0100

    Add product highlights as component

---
+++ components/widgets/productHighlights.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="mx-auto px-[165px] pt-[120px] pb-[168px]">
+    <div class="w-[350px] h-[284px]"></div>
+    <div class="w-[350px] h-[284px]"></div>
+    <div class="w-[350px] h-[284px]"></div>
+  </section>
+</template>
+
+<style scoped></style>
