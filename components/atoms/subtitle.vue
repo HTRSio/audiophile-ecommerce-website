6388fdd1ba0d5cf525e848f18049d1fbf47bc22b
HTRSio <info@htrs.io>
2023-03-03 20:50:20 +0100

    Add subtitle as component

---
+++ components/atoms/subtitle.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const props = defineProps({
+  tag: {
+    type: String,
+    default: "div",
+    validator(value) {
+      return ["span", "div", "h3", "h4", "h6"].includes(value);
+    },
+  },
+});
+</script>
+
+<template>
+  <component :is="props.tag">
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+span,
+div,
+h3,
+h4,
+h5 {
+  @apply text-13 text-primary tracking-1 uppercase;
+}
+</style>
