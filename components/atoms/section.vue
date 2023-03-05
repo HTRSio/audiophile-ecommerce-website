87303c8b6724465f33f7badfeead2eea39ee6c3c
HTRSio <info@htrs.io>
2023-03-05 09:46:16 +0100

    Add default section for the body area

---
+++ components/atoms/section.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section
+    class="mx-auto max-w-screen-mobile tablet:max-w-screen-tablet desktop:max-w-screen-desktop"
+  >
+    <slot />
+  </section>
+</template>
+
+<style scoped></style>
