073a3b9718dc7cd4b3d6a89e7115f7b3e8f8691f
HTRSio <info@htrs.io>
2023-03-05 10:03:13 +0100

    Add tablet and mobile view

--- components/utils/products/categoriesCard.vue
+++ components/utils/products/categoriesCard.vue
@@ -9,9 +9,9 @@ const props = defineProps({
 
 <template>
   <div
-    class="relative w-[21.875rem] h-[12.75rem] flex flex-col items-center "
+    class="relative w-[21.875rem] h-[12.75rem] flex flex-col items-center mt-10"
   >
-    <div class="absolute -top-[5rem] h-56 w-56">
+    <div class="absolute -top-[3rem] desktop:-top-[5rem] h-36 w-36 tablet:h-44 tablet:w-44 desktop:h-56 desktop:w-56">
       <img
         :src="props.item.image"
         width="438"
@@ -20,7 +20,7 @@ const props = defineProps({
         :alt="`category ${props.item.title}`"
       >
     </div>
-    <div class="flex flex-col justify-end items-center bg-gray-light rounded-lg w-full h-[12.75rem]">
+    <div class="flex flex-col justify-end items-center bg-gray-light rounded-lg w-full h-[10rem] desktop:h-[12.75rem]">
       <h2 class="text-18 uppercase font-bold">
         {{ props.item.title }}
       </h2>
--- components/widgets/home/productCategories.vue
+++ components/widgets/home/productCategories.vue
@@ -8,8 +8,8 @@ const props = defineProps({
 </script>
 
 <template>
-  <AtomsSection class="mx-auto pt-[10rem] pb-[10.5rem]">
-    <div class="flex flex-col tablet:flex-row gap-0 tablet:gap-10 w-full">
+  <AtomsSection class="mx-auto pt-[5rem] tablet:pt-[10rem] pb-[10.5rem]">
+    <div class="flex flex-col tablet:flex-row gap-0 tablet:gap-4 desktop:gap-10 w-full">
       <UtilsProductsCategoriesCard
         v-for="(item, index) in props.data" :key="index"
         :item="item"
